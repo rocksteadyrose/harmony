f661c66b8b4d459366ac69c4bbe8411ff9a40a9d
Leslie Rose <leslierose@Leslies-MBP-2.attlocal.net>
2019-05-18 17:29:42 -0500

    php test

--- download.php
+++ download.php
@@ -1,5 +1,5 @@
 <?php if (isset($_GET['file'])) {
-$file = $_GET['file'];
+$file = $_GET['bible.pdf'];
 if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
  header('Content-Type: application/pdf');
  header("Content-Disposition: attachment; filename=\"$file\"");
