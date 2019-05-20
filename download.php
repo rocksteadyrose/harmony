fb93ac2e14d732856511b3a7899b0aa7b7cb202f
Leslie Rose <leslierose@Leslies-MBP-2.attlocal.net>
2019-05-20 15:13:00 -0500

    php new test

--- download.php
+++ download.php
@@ -1,20 +1,5 @@
 <?php
-if(!empty($_GET['images/bible.pdf'])){
-    $fileName = basename($_GET['images/bible.pdf']);
-    $filePath = 'images/bible.pdf'.$fileName;
-    if(!empty($fileName) && file_exists($filePath)){
-        // Define headers
-        header("Cache-Control: public");
-        header("Content-Description: File Transfer");
-        header("Content-Disposition: attachment; filename=$fileName");
-        header("Content-Type: application/zip");
-        header("Content-Transfer-Encoding: binary");
-        
-        // Read the file
-        readfile($filePath);
-        exit;
-    }else{
-        echo 'The file does not exist.';
-    }
-}
+header("Content-disposition: attachment; filename=images/bible.pdf");
+header("Content-type: application/pdf");
+readfile("bible.pdf");
 ?>
